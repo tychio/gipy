a8ed3bdb95f9ba0018c0fe0f3414278c70a74230
Tychio <code@tychio.net>
2016-08-23 09:01:07 +0800

    [feature] - add prefix for comment

--- co.py
+++ co.py
@@ -9,12 +9,21 @@ questions = [
         'commit',
         message = "Please input a comment for your commit"
     ),
+    inquirer.Text(
+        'prefix',
+        message = "Please input a prefix for your commit"
+    ),
 ]
 
 answers = inquirer.prompt(questions)
 
 commitComment = answers['commit']
+prefix = answers['prefix']
+
+if prefix: 
+    prefix = "[" + prefix + "]"
+
 if commitComment:
-    os.system("git commit -m \"" + commitComment + "\"")
+    os.system("git commit -m \"" + prefix + commitComment + "\"")
 else:
-    os.system("git commit -amend")
+    os.system("git commit --amend")
