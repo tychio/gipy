605ef5db66bc2b330e9db3229002022f6f0d33ef
Tychio <code@tychio.net>
2016-09-28 14:29:47 +0800

    recreate repo by pip-init

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="gipy",
+    version="0.1.0",
+    description="A tool for git",
+    license="MIT",
+    author="Tychio",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
